45106395e51590efae7f5f5541c4b8e451fe4552
DONGWON KIM <lunacircle4@gmail.com>
2019-06-16 00:51:18 +0900

    backend Auth

--- Backend/Auth/views.py
+++ Backend/Auth/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, reverse, redirect
 from django.contrib.auth.models import User
-from django.contrib import auth
+from django.contrib import auth, messages
 
 def intro(request):
     return render(request, 'Auth/intro.html')
@@ -14,15 +14,10 @@ def signup(request):
             )
             auth.login(request, newUser)
             return redirect('home')
-            
-    #request.get일때 오류처리를 하거나 signup전용 웹페이지로 코딩할수도 있다.
-    else:
-        return redirect('Auth:intro')
 
 def signin(request):
     if request.method == 'POST':
-        #request.GET.get('next')를 통해 로그인후에 띄울 웹페이지를 제어한다.
-        next=request.GET.get('next')
+        next=request.GET.get("next")
         user = auth.authenticate(
             request,
             username=request.POST['username'],
@@ -35,12 +30,11 @@ def signin(request):
             else:
                 return redirect(next)
         elif next == '':
-            return redirect('home')
+            messages.error(request,'username or password not correct')
+            return redirect('/')
         else:
+            messages.error(request,'username or password not correct')
             return redirect('/?next=' + next)
-    #request.get일때 오류처리를 하거나 signin전용 웹페이지로 코딩할수도 있다.
-    else:
-        return redirect('home')
 
 def signout(request):
     auth.logout(request)
